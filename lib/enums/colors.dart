46dbd9c3b84d09d1b76411e33a1aa23bcf2035f8
Rerurate514 <rerurateyuto@gmail.com>
2024-07-04 20:52:06 +0900

    色の追加

--- lib/enums/colors.dart
+++ lib/enums/colors.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 enum MyColors{
-  PRIMARY_BLUE
+  PRIMARY_BLUE,
+  UNSELECTED_BLUE
 }
 
 extension ColorEx on MyColors{
   Color get color {
     switch(this){
       case MyColors.PRIMARY_BLUE: return const Color.fromARGB(255, 44, 232, 245);
+      case MyColors.UNSELECTED_BLUE: return const Color.fromARGB(255, 14, 68, 71);
     }
   }
 }
