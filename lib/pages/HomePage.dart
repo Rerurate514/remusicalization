a1796519ae80a25dc2485359b1beca19bab19d24
Rerurate514 <rerurateyuto@gmail.com>
2024-07-04 23:10:51 +0900

    HomePageの概形作成

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:musicalization/Widgets/HeaderMenuBar.dart';
+import 'package:musicalization/Widgets/PageWrapper.dart';
+import 'package:musicalization/Widgets/standardSpace.dart';
+import 'package:musicalization/components/HomePage/HomeMusicList.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,9 +12,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  void _onUpdateBtnTapped(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Container();
+    return Scaffold(
+      body: PageWrapper(
+        child: Column(
+          children: [
+            HeaderMenuBar(
+              leftWidget: const Icon(
+                Icons.shuffle,
+                size: 40,
+              ), 
+              leftWidgetTappedCallback: () {}, 
+              rightWidget: const Icon(
+                Icons.add_to_drive_outlined,
+                size: 40,
+              ), 
+              rightWidgetTappedCallback: () {}
+            ),
+            StandardSpace(),
+            const HomeMusicList()
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).cardColor,
+        onPressed: _onUpdateBtnTapped,
+        child: const Icon(
+          Icons.update,
+          size: 32,
+        ),
+      ),
+    );
   }
 }
