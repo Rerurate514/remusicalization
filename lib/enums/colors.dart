8305eeca5c0a712efaae2dcd177baca3ea2bfc52
Rerurate514 <rerurateyuto@gmail.com>
2024-07-04 22:44:48 +0900

    const修飾子の追加

--- lib/enums/colors.dart
+++ lib/enums/colors.dart
@@ -10,7 +10,7 @@ extension ColorEx on MyColors{
   Color get color {
     switch(this){
       case MyColors.PRIMARY_BLUE: return const Color.fromARGB(255, 44, 232, 245);
-      case MyColors.UNSELECTED_BLUE: return Color.fromARGB(255, 22, 103, 107);
+      case MyColors.UNSELECTED_BLUE: return const Color.fromARGB(255, 22, 103, 107);
       case MyColors.CARD_BACKGROUND: return const Color.fromARGB(255, 51, 51, 51); 
     }
   }
