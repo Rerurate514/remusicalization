47ba1d4f5bf745a7a66118183240b4a6445453d4
Rerurate514 <rerurateyuto@gmail.com>
2024-07-05 00:11:30 +0900

    起動時にファイルフェッチする機能実装

--- lib/logic/startup.dart
+++ lib/logic/startup.dart
@@ -1,9 +1,12 @@
 import 'package:musicalization/logic/permission.dart';
+import 'package:musicalization/logic/realm/musicUpdater.dart';
 
 class StartUp{
   final _permissionRequester = MediaAudioPermissionRequester();
-  
+  final _updater = MusicUpdater();
+
   void init(){
     _permissionRequester.requestPermission();
+    _updater.updateDataBase();
   }
 }
