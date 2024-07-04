6db7ea7e5110665ee5a3b74d9ccb249a3205dd6a
Rerurate514 <rerurateyuto@gmail.com>
2024-07-04 19:57:21 +0900

    ファイルフェッチのコード作成

---
+++ lib/logic/logic/fileFetcher.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'filrStrTrimer.dart';
+
+class FileFetcher{
+  late List _list = [];
+  List get list => _list;
+
+  List get nameList => _getNameList();
+  List get pathList => _getPathList();
+
+  final _trimFileStr = FileStrTrimer();
+
+  FileFetcher(){
+    _initFileFetcher();
+  }
+
+  Future<void> _initFileFetcher() async {
+    await _fetchFileFromDownloadDir().then((value) => _list = value);
+  }
+
+  List _getNameList(){
+    _initFileFetcher();
+    return _trimFileStr.convertFileNameToNameString(_list);
+  }
+
+  List _getPathList(){
+    _initFileFetcher();
+    return _trimFileStr.convertFileNameToPathString(_list);
+  }
+
+  ///このメソッドは外部ストレージのオブジェクトを取得するメソッドです。
+  Future<Directory> _fetchExternalDir() async {
+    Directory? directory = Directory("");
+
+    if(Platform.isWindows){
+      //todo
+      directory = Directory("C:\\Users\\rerur\\.pic");
+    }
+    else if(Platform.isAndroid){
+      directory = Directory("/storage/emulated/0/Download/");
+    }
+    else if(Platform.isIOS){
+      //todo
+    }
+    else{
+      print("unknown platform, undefined logic");
+    }
+    
+    return directory;
+  }
+
+  ///このメソッドは外部ストレージのdownloadディレクトリ内のファイルを取得するメソッドです。
+  Future<List> _fetchFileFromDownloadDir() async {
+    Directory dir = await _fetchExternalDir();
+
+    List<FileSystemEntity> result = dir.listSync();
+
+    return result;
+  }
+}
---
+++ lib/logic/logic/filrStrTrimer.dart
@@ -0,0 +1,44 @@
+class FileStrTrimer{
+  List<String> convertFileNameToNameString(List listArg){
+    List<String> strList = convertFileNameToString(listArg);
+    List<String> result = [];
+
+    for(var i = 0; i < strList.length; i++){
+      result.add(
+        strList[i]
+          .replaceAll("File: '/storage/emulated/0/Download/", "")
+          .replaceAll("Directory: '/storage/emulated/0/Download/", "")
+          .replaceAll("'", "")
+      );
+    }
+
+    return result;
+  }
+
+  List<String> convertFileNameToString(List listArg){
+    List<String> result = [];
+
+    for(var i = 0; i < listArg.length; i++){
+      if(listArg[i].toString().contains(".trashed-") || listArg[i].toString().contains("Nearby Share")) continue;
+      result.add(listArg[i].toString());
+    }
+
+    return result;
+  }
+
+  List<String> convertFileNameToPathString(List listArg){
+    List<String> strList = convertFileNameToString(listArg);
+    List<String> result = [];
+
+    for(var i = 0; i < strList.length; i++){
+      result.add(
+        strList[i]
+          .replaceAll("File: '", "")
+          .replaceAll("Directory: '", "")
+          .replaceAll("'", "")
+      );
+    }
+
+    return result;
+  }
+}
