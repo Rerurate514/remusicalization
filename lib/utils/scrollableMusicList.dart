a08cc4fe31e13637e8a1078cff70df26a24a4d40
Rerurate514 <rerurateyuto@gmail.com>
2024-07-06 19:09:03 +0900

    scrollableMusicListウィジェットの作成

---
+++ lib/utils/scrollableMusicList.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:musicalization/Widgets/InkCard.dart';
+import 'package:musicalization/logic/pictureBinaryConverter.dart';
+import 'package:musicalization/models/schema.dart';
+
+class ScrollableMusicList extends StatefulWidget {
+  final List<Music> list;
+  const ScrollableMusicList({super.key, required this.list});
+
+  @override
+  _ScrollableMusicListState createState() => _ScrollableMusicListState();
+}
+
+class _ScrollableMusicListState extends State<ScrollableMusicList> {
+  final ScrollController _scrollController = ScrollController();
+  final PictureBinaryConverter _converter = PictureBinaryConverter();
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Expanded( 
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: widget.list.length,
+        addAutomaticKeepAlives: true,
+        itemBuilder: (_, int index){
+          return InkCard(
+            onTap: () {}, 
+            child: ListTile(
+              leading: Text("leading"),
+              title: Text(widget.list[index].name),
+            )
+          );
+        }
+      ),
+    );
+  }
+
+  Widget buildImage(int index){
+    return widget.list[index].picture != ""
+      ? _buildMusicPicture(widget.list[index])
+      // : Image.asset(
+      //   _picture.musicRecordImg,
+      //   width: 50,
+      : const Icon(Icons.image);
+  }
+
+  Widget _buildMusicPicture(Music music) {
+    ImageProvider image = _converter.convertBase64ToImage(music.picture);
+    return CircleAvatar(
+      foregroundImage: image,
+    );
+  }
+}
