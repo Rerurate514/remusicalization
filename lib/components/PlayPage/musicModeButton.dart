4da3d5dd58eb96a618172e907600588aef11ee46
Rerurate514 <rerurateyuto@gmail.com>
2024-07-11 18:17:34 +0900

    musicModeの切り替えボタンの実装

---
+++ lib/components/PlayPage/musicModeButton.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:musicalization/Widgets/InkCard.dart';
+import 'package:musicalization/enums/colors.dart';
+import 'package:musicalization/enums/musicMode.dart';
+import 'package:musicalization/logic/musicPlayer.dart';
+
+class MusicModeButton extends StatefulWidget {
+  const MusicModeButton({super.key});
+
+  @override
+  _MusicModeButtonState createState() => _MusicModeButtonState();
+}
+
+class _MusicModeButtonState extends State<MusicModeButton> {
+  final MusicPlayer _player = MusicPlayer.getEmptyInstance();
+
+  final Widget normalIcon = Icon(
+    Icons.loop,
+    color: MyColors.UNSELECTED_BLUE.color,
+    size: 32,
+  );
+
+  final Widget loopIcon = const Icon(
+    Icons.loop,
+    size: 32,
+  );
+
+  final Widget shuffleIcon = const Icon(
+    Icons.shuffle,
+    size: 32,
+  );
+
+  Widget _icon = Container();
+
+  @override
+  void initState(){
+    super.initState();
+    _icon = normalIcon;
+  }
+
+  void onTapped(){
+    setState(() {
+      switch(_player.musicMode){
+        case MusicMode.NORMAL: {
+          _icon = loopIcon;
+        }
+        case MusicMode.LOOP: {
+          _icon = shuffleIcon;
+        }
+        case MusicMode.SHUFFLE: {
+          _icon = normalIcon;
+        }
+      }
+    });
+
+    _player.toggleMusicMode();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return InkCard(
+      onTap: onTapped, 
+      padding: 10,
+      child: _icon
+    );
+  }
+}
