abb4e6dd7ef34a5298a5d2420c91ea39c6717ba7
Rerurate514 <rerurateyuto@gmail.com>
2024-08-05 00:02:31 +0900

    歌詞表示ボタンの追加

---
+++ lib/components/PlayPage/lyricsShowFloatingButton.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:musicalization/providers/showLyricsProvider.dart';
+
+class LyricsShowFloatingButton extends ConsumerStatefulWidget{
+  const LyricsShowFloatingButton({super.key});
+
+  @override
+  LyricsShowFloatingButtonState createState() => LyricsShowFloatingButtonState();
+}
+
+class LyricsShowFloatingButtonState extends ConsumerState<LyricsShowFloatingButton>{
+    void onTapped(){
+    ref.read(showLyricsProvider.notifier).state = 
+    !ref.read(showLyricsProvider.notifier).state;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    final Size size = MediaQuery.of(context).size;
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Padding(
+        padding: EdgeInsets.only(top: size.height * 0.14, left: size.width * 0.072),
+        child: FloatingActionButton(
+          onPressed: () {
+            
+          },
+          backgroundColor: Theme.of(context).cardColor,
+          child: const Icon(
+            Icons.lyrics
+          )
+        )
+      ),
+    );
+  }
+}
