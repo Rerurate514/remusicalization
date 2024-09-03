c4bb62b50ff254607239cd7f950c844f86e66b53
Rerurate514 <rerurateyuto@gmail.com>
2024-09-03 20:11:02 +0900

    ファクトリを静的に変更

--- lib/models/wrappedPlayList.dart
+++ lib/models/wrappedPlayList.dart
@@ -5,7 +5,7 @@ class WrappedPlayList extends PlayList{
   final List<Music> musicList;
   WrappedPlayList._(super.id, super.name, this.musicList);
 
-  Future<WrappedPlayList> getInstance(PlayList playList) async {
+  static Future<WrappedPlayList> getInstance(PlayList playList) async {
     return WrappedPlayList._(
       playList.id,
       playList.name,
