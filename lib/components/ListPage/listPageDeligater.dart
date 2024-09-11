3d80cc74e893db867d85fd77c9402f8892ac652c
Rerurate514 <rerurateyuto@gmail.com>
2024-09-11 17:35:54 +0900

    musicListを受け渡すように調整

--- lib/components/ListPage/listPageDeligater.dart
+++ lib/components/ListPage/listPageDeligater.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:musicalization/components/ListPage/choiceMusic.dart';
 import 'package:musicalization/components/ListPage/choicePlayList.dart';
 import 'package:musicalization/providers/isPlayListSelectedProvider.dart';
+import 'package:musicalization/providers/musicListInPlayListProvider.dart';
 
 class ListPageDeligater extends ConsumerStatefulWidget {
   const ListPageDeligater({super.key});
@@ -16,8 +17,10 @@ class ListPageDeligaterState extends ConsumerState<ListPageDeligater>{
   @override
   Widget build(BuildContext context){
     final isPlayListSelected = ref.watch(isPlayListSelectedProvider);
+    final musicList = ref.watch(musicListInPlayListProvider.notifier).state;
+    
     return isPlayListSelected
-    ? const ChoiceMusic()
+    ? ChoiceMusic(list: musicList)
     : const ChoicePlayList();
   }
 }
