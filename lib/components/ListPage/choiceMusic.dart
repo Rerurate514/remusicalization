f7f53a21434825b455608776b07bd225a4d8bcb7
Rerurate514 <rerurateyuto@gmail.com>
2024-09-16 18:23:02 +0900

    choiceMusicUIの作成

---
+++ lib/components/ListPage/choiceMusic.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/material.dart';
+import 'package:musicalization/Widgets/HeaderMenuBar.dart';
+import 'package:musicalization/Widgets/standardSpace.dart';
+import 'package:musicalization/components/HomePage/ScrollableMusicList.dart';
+import 'package:musicalization/logic/recordFetcher.dart';
+import 'package:musicalization/models/schema.dart';
+
+class ChoiceMusic extends StatefulWidget {
+  final List<Music> list;
+
+  const ChoiceMusic({super.key, required this.list});
+
+  @override
+  ChoiceMusicState createState() => ChoiceMusicState();
+}
+
+class ChoiceMusicState extends State<ChoiceMusic>{
+  final RecordFetcher<Music> _fetcher = RecordFetcher<Music>(Music.schema);
+
+  @override
+  void initState(){
+    super.initState();
+
+    setState(() {
+      
+    });
+  }
+  
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: [
+          StandardSpace(),
+          HeaderMenuBar(
+            leftWidget: const Icon(
+              Icons.shuffle,
+              size: 40,
+            ), 
+            leftWidgetTappedCallback: () {}, 
+            rightWidget: const Icon(
+              Icons.playlist_add,
+              size: 40,
+            ), 
+            rightWidgetTappedCallback: () {}
+          ),
+          StandardSpace(),
+          ScrollableMusicList(list: widget.list)
+        ],
+      ),
+    );
+  }
+}
