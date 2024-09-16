80af2c98ef42de41b6dbd46cf7882c6e9975bfca
Rerurate514 <rerurateyuto@gmail.com>
2024-09-16 20:08:57 +0900

    paddingの調整

--- lib/components/ListPage/returnButtonFromMusic.dart
+++ lib/components/ListPage/returnButtonFromMusic.dart
@@ -20,7 +20,7 @@ class ReturnButtonFromMusicState extends ConsumerState<ReturnButtonFromMusic>{
     final prov = ref.watch(isPlayListSelectedProvider);
     final Size size = MediaQuery.of(context).size;
     return Padding(
-      padding: EdgeInsets.only(left: size.width * 0.05),
+      padding: EdgeInsets.only(left: size.width * 0.06),
       child: prov
         ? FloatingActionButton(
           backgroundColor: MyColors.CARD_BACKGROUND.color,
