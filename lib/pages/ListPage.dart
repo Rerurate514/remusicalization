0436701a9e4f448d2f1dde45e2fd0f28522fecd4
Rerurate514 <rerurateyuto@gmail.com>
2024-09-16 18:23:12 +0900

    ListPageUIの作成

--- lib/pages/ListPage.dart
+++ lib/pages/ListPage.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:musicalization/Widgets/HeaderMenuBar.dart';
 import 'package:musicalization/Widgets/MyAppBar.dart';
 import 'package:musicalization/Widgets/PageWrapper.dart';
-import 'package:musicalization/Widgets/scrollableMusicList.dart';
-import 'package:musicalization/Widgets/standardSpace.dart';
-import 'package:musicalization/components/ListPage/registerListDialog.dart';
-import 'package:musicalization/components/ListPage/registerListNameDialog.dart';
-import 'package:musicalization/logic/recordFetcher.dart';
-import 'package:musicalization/models/schema.dart';
-import 'package:realm/realm.dart';
-import 'package:musicalization/settings/globalNavigatoeKey.dart';
+import 'package:musicalization/components/ListPage/listPageDeligater.dart';
+import 'package:musicalization/components/ListPage/returnButtonFromMusic.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({super.key});
@@ -19,28 +12,9 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  List<Music> _list = [];
-
-  final _recordFetcher = RecordFetcher<Music>(Music.schema);
-
-  @override
-  void initState(){
-    super.initState();
-
-    setState(() {
-      
-    });
-  }
-
   void _onUpdateBtnTapped(){
 
   }
-
-  void _onListAddBtnTapped() async {
-
-  }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -49,35 +23,23 @@ class _ListPageState extends State<ListPage> {
       appBar: const MyAppBar(
         image: AssetImage("assets/images/mp3_mode_list.png")
       ),
-      body: PageWrapper(
-        child: Column(
-          children: [
-            StandardSpace(),
-            HeaderMenuBar(
-              leftWidget: const Icon(
-                Icons.shuffle,
-                size: 40,
-              ), 
-              leftWidgetTappedCallback: () {}, 
-              rightWidget: const Icon(
-                Icons.playlist_add,
-                size: 40,
-              ), 
-              rightWidgetTappedCallback: _onListAddBtnTapped
+      body: const PageWrapper(
+        child: ListPageDeligater() 
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const ReturnButtonFromMusic(),
+          FloatingActionButton(
+            backgroundColor: Theme.of(context).cardColor,
+            onPressed: _onUpdateBtnTapped,
+            child: const Icon(
+              Icons.update,
+              size: 32,
             ),
-            StandardSpace(),
-            ScrollableMusicList(list: _list)
-          ],
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).cardColor,
-        onPressed: _onUpdateBtnTapped,
-        child: const Icon(
-          Icons.update,
-          size: 32,
-        ),
-      ),
+          ),
+        ],
+      )
     );
   }
 }
