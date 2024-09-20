876e70eb143f63c8a646a48075dcf7d64ef2570e
Rerurate514 <rerurateyuto@gmail.com>
2024-09-21 08:27:05 +0900

    startUpRepoの削除

--- lib/logic/startup.dart
+++ lib/logic/startup.dart
@@ -1,12 +1,26 @@
+import 'package:musicalization/logic/fileFetcher.dart';
+import 'package:musicalization/logic/musicCreater.dart';
 import 'package:musicalization/logic/permission.dart';
-import 'package:musicalization/logic/startUpRepositry.dart';
+import 'package:musicalization/logic/realm/realmUpdater.dart';
 
 class StartUp{
   final _mediaAudioRequester = MediaAudioPermissionRequester();
-  final _repo = StartUpRepositry();
+  final _fetcher = FileFetcher();
+  final _creater = MusicCreater();
+  final _updater = Updater();
 
   void init() async {
     await _mediaAudioRequester.requestPermission();
-    _repo.refreshData();
+    _refreshData();
   }
+
+
+  void _refreshData() async {
+    final pathList = await _fetcher.pathList;
+    final nameList = await _fetcher.nameList;
+
+    final musicList = _creater.generateMusicList(pathList, nameList);
+
+    _updater.update(musicList);
+  } 
 }
