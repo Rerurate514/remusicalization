6e446d0cac030671ebc8fe5a6665785b10a89b25
Rerurate514 <rerurateyuto@gmail.com>
2024-11-20 01:43:29 +0900

    コードの簡素化

--- lib/logic/startup.dart
+++ lib/logic/startup.dart
@@ -1,12 +1,8 @@
-import 'package:musicalization/logic/fileFetcher.dart';
-import 'package:musicalization/logic/musicCreater.dart';
 import 'package:musicalization/logic/permission.dart';
 import 'package:musicalization/logic/realm/realmUpdater.dart';
 
 class StartUp{
   final _mediaAudioRequester = MediaAudioPermissionRequester();
-  final _fetcher = FileFetcher();
-  final _creater = MusicCreater();
   final _updater = Updater();
 
   void init() async {
@@ -16,11 +12,6 @@ class StartUp{
 
 
   void _refreshData() async {
-    final pathList = await _fetcher.pathList;
-    final nameList = await _fetcher.nameList;
-
-    final musicList = _creater.generateMusicList(pathList, nameList);
-
-    await _updater.update(musicList);
+    await _updater.update();
   } 
 }
