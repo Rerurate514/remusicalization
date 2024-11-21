07a3021438dee2ff6a98c540b7408325d48144c6
Rerurate514 <rerurateyuto@gmail.com>
2024-11-21 20:59:39 +0900

    listShowerをジェネリックに対応

--- lib/utils/listShower.dart
+++ lib/utils/listShower.dart
@@ -1,7 +1,7 @@
-extension ListShower on List {
+extension ListShower<T> on List<T> {
   void printLog(){
     if(this.isEmpty) print("List is empty");
-    this.forEach((var ele){
+    this.forEach((T ele){
       print("showList = $ele");
     });
   }
