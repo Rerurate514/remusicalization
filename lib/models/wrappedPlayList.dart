532ed0532b2e455278fb204ff4b3c4f0adfb246a
Rerurate514 <rerurateyuto@gmail.com>
2025-03-23 20:24:14 +0900

    wrapperの内部に実際のオブジェクトを注入

--- lib/models/wrappedPlayList.dart
+++ lib/models/wrappedPlayList.dart
@@ -3,14 +3,22 @@ import 'package:musicalization/models/schema.dart';
 
 class WrappedPlayList extends PlayList{
   final List<Music> musicList;
-  WrappedPlayList._(super.id, super.name, super.picture, this.musicList);
+  final PlayList playListModel;
+  WrappedPlayList._(
+    super.id, 
+    super.name, 
+    super.picture, 
+    this.musicList, 
+    this.playListModel
+  );
 
   static Future<WrappedPlayList> getInstance(PlayList playList) async {
     return WrappedPlayList._(
       playList.id,
       playList.name,
       playList.picture,
-      await playList.list.toMusicList()
+      await playList.list.toMusicList(),
+      playList
     );
   }
 }
