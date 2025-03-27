8ecfcd7ec13b8325ba26f91447fcbbfa90d2ded4
Rerurate514 <rerurateyuto@gmail.com>
2025-03-28 00:09:40 +0900

    権限をリクエストするボタンの追加

--- lib/pages/SettingPage.dart
+++ lib/pages/SettingPage.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:musicalization/Widgets/InkCard.dart';
 import 'package:musicalization/Widgets/MyAppBar.dart';
 import 'package:musicalization/Widgets/PageWrapper.dart';
+import 'package:musicalization/logic/permission.dart';
+import 'package:musicalization/utils/showSuccessDialog.dart';
+import 'package:musicalization/utils/showWarnDialog.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -19,17 +23,41 @@ class _SettingPageState extends State<SettingPage> {
       ),
       body: PageWrapper(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            InkCard(
-              padding: 10,
-              onTap: () {
-                showLicensePage(context: context);
-              },
-              child: const Text("LICENCE"),
-            )
+            _buildLicenceCard(),
+            _buildPermissionRequestButton()
           ],
         ),
       )
+    );
+  }
+
+  Widget _buildLicenceCard(){
+    return InkCard(
+      padding: 10,
+      onTap: () {
+        showLicensePage(context: context);
+      },
+      child: const Text("LICENCE"),
+    );
+  }
+
+  Widget _buildPermissionRequestButton(){
+    return InkCard(
+      padding: 10,
+      onTap: () async {
+        final media = await MediaAudioPermissionRequester().requestPermission();
+        final storage = await StorageAccessRequester().requestPermission();
+
+        if(media.isGranted && storage.isGranted){
+          showSuccessDialog("権限が全て認証されています！");
+        }
+        else{
+          showWarnDialog("必要な権限が足りていません。");
+        }
+      }, 
+      child: const Text("権限を承認する。")
     );
   }
 }
