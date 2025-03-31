eb1399436a297ea241c19ed870b5cfa470906c9c
Rerurate514 <rerurateyuto@gmail.com>
2025-03-31 10:40:56 +0900

    MusicImageが曲の切り替えごとに切り替わるように修正

--- lib/components/PlayPage/nextMusicButton.dart
+++ lib/components/PlayPage/nextMusicButton.dart
@@ -2,23 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:musicalization/Widgets/InkCard.dart';
 import 'package:musicalization/enums/transition.dart';
 import 'package:musicalization/logic/musicPlayer.dart';
+import 'package:musicalization/logic/pictureBinaryConverter.dart';
 import 'package:musicalization/logic/sizeDiffFns.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:musicalization/providers/musicImageProvider.dart';
 
-class NextMusicButton extends StatelessWidget{
-  final _player = MusicPlayer.getEmptyInstance();
-
-  NextMusicButton({super.key});
+class NextMusicButton extends ConsumerStatefulWidget {
+  const NextMusicButton({super.key});
 
   @override
-  Widget build(BuildContext context){
+  ConsumerState<NextMusicButton> createState() => _NextMusicButtonState();
+}
+
+class _NextMusicButtonState extends ConsumerState<NextMusicButton> {
+  final _player = MusicPlayer.getEmptyInstance();
+  final PictureBinaryConverter _converter = PictureBinaryConverter();
+
+  void _setMusicPictureToProv() {
+    ref.read(musicImageProvider.notifier).update(
+      (state) => _player.currentMusic.picture != "" 
+      ? _converter.convertBase64ToImage(_player.currentMusic.picture)
+      : null
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return InkCard(
       onTap: () {
         _player.moveMusicList(Transition.NEXT);
-      }, 
+        _setMusicPictureToProv();
+      },
       padding: 10,
       child: Icon(
         Icons.skip_next,
-        size: getButtonsSize()
+        size: getButtonsSize(),
       )
     );
   }
--- lib/components/PlayPage/previousButton.dart
+++ lib/components/PlayPage/previousButton.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:musicalization/Widgets/InkCard.dart';
 import 'package:musicalization/enums/transition.dart';
 import 'package:musicalization/logic/musicPlayer.dart';
+import 'package:musicalization/logic/pictureBinaryConverter.dart';
 import 'package:musicalization/logic/sizeDiffFns.dart';
+import 'package:musicalization/providers/musicImageProvider.dart';
 
-class PreviousMusicButton extends StatelessWidget{
-  final _player = MusicPlayer.getEmptyInstance();
-
-  PreviousMusicButton({super.key});
+class PreviousMusicButton extends ConsumerStatefulWidget {
+  const PreviousMusicButton({super.key});
 
   @override
-  Widget build(BuildContext context){
+  ConsumerState<PreviousMusicButton> createState() => _PreviousMusicButtonState();
+}
+
+class _PreviousMusicButtonState extends ConsumerState<PreviousMusicButton> {
+  final _player = MusicPlayer.getEmptyInstance();
+  final PictureBinaryConverter _converter = PictureBinaryConverter();
+
+  void _setMusicPictureToProv() {
+    ref.read(musicImageProvider.notifier).update(
+      (state) => _player.currentMusic.picture != "" 
+      ? _converter.convertBase64ToImage(_player.currentMusic.picture)
+      : null
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return InkCard(
       onTap: () {
         _player.moveMusicList(Transition.PREVIOUS);
-      }, 
+        _setMusicPictureToProv();
+      },
       padding: 10,
-      child:  Icon(
+      child: Icon(
         Icons.skip_previous,
         size: getButtonsSize(),
       )
